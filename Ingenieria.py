fc4c805801faacf3f7d00758f29e47f3a216ed55
Cristian Turcios <Cristian Turcios>
2017-08-05 19:39:57 -0600

    delete funny vars

--- Ingenieria.py
+++ Ingenieria.py
@@ -3,5 +3,5 @@ class Ingenieria(Students, object):
     """docstring for Ingenieria."""
 
     def printFullNameUpperCase(self):
-        pollosLocos = super(Ingenieria, self).getAllDataOfStudent()
-        return pollosLocos.upper()
+        allData = super(Ingenieria, self).getAllDataOfStudent()
+        return allData.upper()
